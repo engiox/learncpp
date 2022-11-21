4c30b130802c8fdc6a603e85c392dafca33584e3
Me <cryoon@deviceeng.co.kr>
2022-11-21 18:02:17 +0900

    WIP

---
+++ 5.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int getValue()
+{
+    std::cout << "Enter an integer: ";
+
+    int x{};
+    std::cin >> x;
+    return x;
+}
+
+int main()
+{
+    std::cout << getValue() + (getValue() * getValue()); // a + (b * c)
+    return 0;
+}
