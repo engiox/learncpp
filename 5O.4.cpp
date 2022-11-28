620f8d39e2db811effb996bacce596b6fa83bd51
Me <cryoon@deviceeng.co.kr>
2022-11-28 17:41:04 +0900

    WIP

--- 5O.4.cpp
+++ 5O.4.cpp
@@ -5,10 +5,11 @@
 
 int main()
 {
-    for (uint8_t i = 0b0000'0000; i <= 0b1111'1111; ++i) {
-        std::cout << static_cast<std::bitset<8>>(i) << " : "
-            << static_cast<int>(i) << " : " << static_cast<unsigned>(i)
+    for (uint8_t i = 0b0000'0000; ; ++i) {
+        std::cout << static_cast<std::bitset<8>>(i) << " : " << std::setw(3)
+            << static_cast<int>(i) << " : " << std::setw(3) << static_cast<unsigned>(i)
             << ((i % 4 == 0) ? '\n' : '\t');
+        if (i == 0b1111'1111) break;
     }
     return 0;
 }
