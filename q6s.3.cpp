fa5abf26727812c45f489297e92fa574936394c2
Me <cryoon@deviceeng.co.kr>
2022-12-06 18:08:12 +0900

    WIP

---
+++ q6s.3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "User #1: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #2: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #3: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #4: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #5: " << (passOrFail() ? "Pass\n" : "Fail\n");
+
+	return 0;
+}
