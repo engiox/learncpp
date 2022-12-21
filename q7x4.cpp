cd45f08507c850e238e84052fd91ab8823c54836
engiox <engio@naver.com>
2022-12-21 12:56:18 +0900

    WIP

--- q7x4.cpp
+++ q7x4.cpp
@@ -1,73 +1,41 @@
+#include <ios>
 #include <iostream>
 #include <random>
 #include <limits>
 
-void gameIntro()
+void ignoreLine()
 {
-    std::cout << "Let's play a game. I'm thinking of a number between 1 and 100.\n"
-        "You have 7 tries to guess what it is.\n";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
-namespace Random
+int getInteger()
 {
-    std::random_device rd;
-    std::seed_seq ss{ rd(), rd(), rd(), rd(), rd(), rd() };
-    std::mt19937 mt{ ss };
-    std::uniform_int_distribution<> answerRange{ 1, 100 };
+    while (true) {
+        int input{};
+        std::cin >> input;
 
-    int getRandom()
-    {
-        return answerRange(Random::mt);
+        if (!std::cin) {
+            std::cin.clear();
+            ignoreLine();
+            std::cerr << "Wrong input. Integer required.\n";
+        } else {
+            ignoreLine();
+            return input;
+        }
     }
 }
 
-bool mainGame(int answer)
+bool playGame(int guess, int number)
 {
-    int guess{};
-    for (int chance{ 0 }; chance < 7; ++chance) {
-        std::cout << "Guess #" << chance << ": ";
-        std::cin >> guess;
-        if (guess == answer) {
-            std::cout << "Correct! You Win!\n";
-            return true;
-        }
-        if (guess > answer) {
-            std::cout << "Your guess is too high.\n";
-            continue;
-        }
-        std::cout << "Your guess is too low.\n";
-    }
-    std::cout << "Sorry, you lose. The correct number was " << answer << ".\n";
     return false;
 }
 
-bool continueGameQ()
+bool playAgain()
 {
-    while (true) {
-        std::cout << "Would you like to play again (y/n) ? ";
-        char yorn{};
-        std::cin >> yorn;
-        switch (yorn) {
-            case 'y':
-                return true;
-            case 'n':
-                return false;
-            default:
-                continue;
-        }
-    }
+    return false;
 }
 
 int main()
 {
-    while (true) {
-        gameIntro();
-        int answer { Random::getRandom() };
-
-        mainGame(answer);
-        if (!continueGameQ())
-            return 0;
-    }
-
     return 0;
 }
