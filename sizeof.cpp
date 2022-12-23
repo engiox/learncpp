0979d28b5076a0cae5bb5f0792919f34008df9c2
engiox <engio@naver.com>
2022-12-23 15:37:37 +0900

    WIP

--- sizeof.cpp
+++ sizeof.cpp
@@ -9,10 +9,14 @@ int main()
     std::cout << "char32_t:\t" << sizeof(char32_t) << " bytes\n";
     std::cout << "short:\t\t" << sizeof(short) << " bytes\n";
     std::cout << "int:\t\t" << sizeof(int) << " bytes\n";
+    std::cout << "666 :\t\t" << sizeof(666) << " bytes\n";
     std::cout << "long:\t\t" << sizeof(long) << " bytes\n";
+    std::cout << "666l:\t\t" << sizeof(666l) << " bytes\n";
     std::cout << "long long:\t" << sizeof(long long) << " bytes\n";
     std::cout << "float:\t\t" << sizeof(float) << " bytes\n";
+    std::cout << "3.14f:\t\t" << sizeof(3.14f) << " bytes\n";
     std::cout << "double:\t\t" << sizeof(double) << " bytes\n";
+    std::cout << "3.1415:\t\t" << sizeof(3.1415) << " bytes\n";
     std::cout << "long double:\t" << sizeof(long double) << " bytes\n";
     std::cout << "pointer:\t" << sizeof(int *) << " bytes\n";
 
