71c5a319982941a4d21f86dc154dd36ccce24a4d
engiox <engio@naver.com>
2022-12-27 17:53:22 +0900

    WIP

---
+++ 8.4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <typeinfo>
+
+int main()
+{
+    int i{ 2 };
+    double d{ 3.5 };
+    std::cout << typeid(i + d).name() << ' ' << i + d << '\n';
+
+    short a{ 4 }, b{ 5 };
+    std::cout << typeid(a + b).name() << ' ' << a + b << '\n';
+
+    std::cout << typeid(5u-10).name() << ' ' << 5u - 10 << '\n';
+
+    return 0;
+}
