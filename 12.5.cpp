ff551c6f9dc9c0712fc6589bbf40d0288216d4ae
engiox <engio@naver.com>
2023-02-22 21:09:48 +0900

    WIP

--- 12.5.cpp
+++ 12.5.cpp
@@ -1,15 +1,28 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <sstream>
 
 int main(int argc, char *argv[])
 {
     std::cout << "There " << (argc > 1 ? "are " : "is " ) << argc
               << " argument" << (argc > 1 ? "s:" : ":") << '\n';
 
-    for (auto count{0}; count < argc; ++count) {
-        std::cout << count << ' ' << argv[count] << '\n';
-    }
+    std::stringstream buff{ argv[1] };
+    int val{};
 
-    if ("") std::cout << "empty string or pointer?\n";
+    if (!(buff >> val))
+        val = 0;
+
+    std::cout << val << " / 2 = " << val/2 << '\n';
+
+//    for (auto count{0}; count < argc; ++count) {
+//        buff << argv[count];
+//        if ((buff >> val))
+//            std::cout << val << " / 2 = " << val/2 << '\n';
+//        else
+//            std::cout << argv[count] << '\n';
+//    }
 
     return 0;
 }
