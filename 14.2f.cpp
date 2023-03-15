c9bb1d8cb2f3d49486559ba8cddbc4e56b11c944
engiox <engio@naver.com>
2023-03-16 01:13:02 +0900

    WIP

---
+++ 14.2f.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <numeric>
+#include "Fraction.h"
+
+Fraction::Fraction(int num, int den) : m_numerator{num}, m_denominator{den}
+{
+    reduce();
+}
+
+void Fraction::print()
+{
+    std::cout << m_numerator << '/' << m_denominator << '\n';
+}
+
+Fraction operator*(const Fraction &f1, const Fraction &f2)
+{
+    return Fraction{f1.m_numerator * f2.m_numerator, f1.m_denominator * f2.m_denominator};
+}
+
+Fraction operator*(const Fraction &f1, int inum)
+{
+    return Fraction{f1.m_numerator * inum, f1.m_denominator};
+}
+
+Fraction operator*(int inum, const Fraction &f1) { return f1 * inum; }
+
+Fraction &Fraction::reduce()
+{
+    int gcd{ std::gcd(m_numerator, m_denominator) };
+    if (gcd) {
+        m_numerator /= gcd;
+        m_denominator /= gcd;
+    }
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &out, const Fraction &fr)
+{
+    out << fr.m_numerator << '/' << fr.m_denominator << '\n';
+    return out;
+}
+
+std::istream &operator>>(std::istream &in, Fraction &fr)
+{
+    in >> fr.m_numerator; in >> fr.m_denominator;
+    return in;
+}
---
+++ 14.2f.h
@@ -0,0 +1,21 @@
+#ifndef _FRACTION_H_
+#define _FRACTION_H_
+
+#include <iostream>
+
+class Fraction
+{
+    int m_numerator{0};
+    int m_denominator{1};
+public:
+    Fraction(int num = 0, int den = 1); // : m_numerator{num}, m_denominator{den} {}
+    void print();
+    Fraction &reduce();
+    friend Fraction operator*(const Fraction &f1, const Fraction &f2);
+    friend Fraction operator*(const Fraction &f1, int inum);
+    friend Fraction operator*(int inum, const Fraction &f1); // { return f1 * inum; }
+    friend std::ostream &operator<<(std::ostream &out, const Fraction &fr);
+    friend std::istream &operator>>(std::istream &in, Fraction &fr);
+};
+
+#endif
