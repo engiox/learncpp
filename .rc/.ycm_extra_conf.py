a14a09a5fc76c6ea35521208734683a6de0de350
engiox <engio@naver.com>
2026-03-26 20:06:23 +0900

    WIP

--- .rc/.ycm_extra_conf.py
+++ .rc/.ycm_extra_conf.py
@@ -27,24 +27,24 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 #
 # For more information, please refer to <http://unlicense.org/>
-# 
+#
 # import os.path as p
 # import subprocess
-# 
+#
 # DIR_OF_THIS_SCRIPT = p.abspath( p.dirname( __file__ ) )
 # DIR_OF_THIRD_PARTY = p.join( DIR_OF_THIS_SCRIPT, 'third_party' )
-# 
-# 
+#
+#
 # def GetStandardLibraryIndexInSysPath( sys_path ):
 #   for index, path in enumerate( sys_path ):
 #     if p.isfile( p.join( path, 'os.py' ) ):
 #       return index
 #   raise RuntimeError( 'Could not find standard library path in Python path.' )
-# 
-# 
+#
+#
 # def PythonSysPath( **kwargs ):
 #   sys_path = kwargs[ 'sys_path' ]
-# 
+#
 #   dependencies = [ p.join( DIR_OF_THIS_SCRIPT, 'python' ),
 #                    p.join( DIR_OF_THIRD_PARTY, 'requests-futures' ),
 #                    p.join( DIR_OF_THIRD_PARTY, 'ycmd' ),
@@ -56,7 +56,7 @@
 #                            'src' ),
 #                    p.join( DIR_OF_THIRD_PARTY, 'requests_deps', 'certifi' ),
 #                    p.join( DIR_OF_THIRD_PARTY, 'requests_deps', 'requests' ) ]
-# 
+#
 #   # The concurrent.futures module is part of the standard library on Python 3.
 #   interpreter_path = kwargs[ 'interpreter_path' ]
 #   major_version = int( subprocess.check_output( [
@@ -64,11 +64,11 @@
 #   ).rstrip().decode( 'utf8' ) )
 #   if major_version == 2:
 #     dependencies.append( p.join( DIR_OF_THIRD_PARTY, 'pythonfutures' ) )
-# 
+#
 #   sys_path[ 0:0 ] = dependencies
 #   sys_path.insert( GetStandardLibraryIndexInSysPath( sys_path ) + 1,
 #                    p.join( DIR_OF_THIRD_PARTY, 'python-future', 'src' ) )
-# 
+#
 #   return sys_path
 
 ##################################################################
@@ -78,5 +78,6 @@ def Settings( **kwargs ):
   return {
     'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', '-Weffc++',
                '-Wsign-conversion', '-Wno-unused-variable', '-Wc++17-extensions',
+               '-ID:/usr/include',
                '-std=c++20' ],
   }
