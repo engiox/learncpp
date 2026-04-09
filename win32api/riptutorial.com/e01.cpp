c1e79d801cf14760dea2a4288b7b6eba2450e87f
Me <cryoon@deviceeng.co.kr>
2026-04-10 07:44:40 +0900

    WIP

--- win32api/riptutorial.com/e01.cpp
+++ win32api/riptutorial.com/e01.cpp
@@ -2,5 +2,9 @@
 
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdLine, int cmdShow)
 {
-    return MessageBox(NULL, "Hello, world!", "caption", 0);
+    char strInst[100];
+    int i = wsprintfA(strInst, "hInst: %x, hInstPrev: %x, *PSTR: %s, cmdShow: %d\n",
+                      hInst, hInstPrev, cmdLine, cmdShow);
+
+    return MessageBox(NULL, strInst, "Hello, world!", 0);
 }
